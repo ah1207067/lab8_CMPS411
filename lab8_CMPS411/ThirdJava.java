8ff24ba81988a835165740f40154e8239c9d9811
Ali <ah1207067@qu.edu.qa>
2015-05-08 18:48:53 +0300

    aa

---
+++ lab8_CMPS411/ThirdJava.java
@@ -0,0 +1,7 @@
+import java.lang.*;  
+  public class ThirdJava {  
+   public static void main(String[] args)  
+  {  System.out.println("This is my third Java program."); 
+      StdOut.print("Type the third integer: ");
+      } }  
+ 
